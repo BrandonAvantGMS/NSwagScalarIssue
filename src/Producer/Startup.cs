676c4e1ef7bc6845a57c7a26496e2bd229b7ecce
Brandon Avant <brandon.avant@gbrx.com>
2021-11-11 15:31:11 -0600

    Removing HTTPS redirection

--- src/Producer/Startup.cs
+++ src/Producer/Startup.cs
@@ -37,12 +37,8 @@ namespace Producer
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Producer v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
